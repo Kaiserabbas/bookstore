3e6187aa16536f3cd18b89d8cca67459baa9d96c
Qaisar <kayser.abbas@gmail.com>
2023-08-28 14:22:31 +0500

    Add AddButton.js

---
+++ src/components/AddButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addBook } from '../redux/books/booksSlice';
+
+const AddButton = ({ title, author }) => {
+  const dispatch = useDispatch();
+
+  const handleAddBook = () => {
+    const newBook = {
+      id: Date.now(),
+      title: title,
+      author: author,
+      category: 'New Book Category',
+      progress: 0,
+      state: 'Chapter 1',
+    };
+    dispatch(addBook(newBook));
+  };
+
+  return (
+    <button type="button" onClick={handleAddBook}>
+      Add Book
+    </button>
+  );
+};
+AddButton.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+export default AddButton;
