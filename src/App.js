07b9b4d8341f6753848c9b3f8737f40ebd6ed55d
Qaisar <kayser.abbas@gmail.com>
2023-08-28 15:56:21 +0500

    <Provider> moved to index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
-import store from './redux/store';
 import Books from './components/Books';
 import Categories from './components/Categories';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
-      </div>
-    </Provider>
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
   );
 }
 
