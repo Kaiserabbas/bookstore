e91b4a8b872a056453e304be6a060a5168c1a86f
Qaisar <kayser.abbas@gmail.com>
2023-08-28 14:23:15 +0500

    Add RemoveButton.js

---
+++ src/components/RemoveButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
+
+const RemoveButton = ({ bookId }) => {
+  const dispatch = useDispatch();
+
+  const handleRemoveBook = () => {
+    dispatch(removeBook(bookId));
+  };
+
+  return (
+    <button type="button" onClick={handleRemoveBook}>
+      Remove <span className="vertical-line">|</span>
+    </button>
+  );
+};
+
+RemoveButton.propTypes = {
+  bookId: PropTypes.number.isRequired,
+};
+
+export default RemoveButton;
