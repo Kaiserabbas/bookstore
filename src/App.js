dd5f4761d3f92953a708bf81563b11e223e3ee47
Qaisar <kayser.abbas@gmail.com>
2023-08-28 14:21:39 +0500

    Wrap <App> with the <Provider> component from react-redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
 import React from 'react';
-import { Provider } from 'react-redux'; // Import the Provider component
+import { Provider } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
-import store from './redux/store'; // Import your Redux store
+import './App.css';
+import Nav from './components/Nav';
+import store from './redux/store';
 import Books from './components/Books';
 import Categories from './components/Categories';
-import Nav from './components/Nav';
 
 function App() {
   return (
     <Provider store={store}>
-      {' '}
-      {/* Wrap your components with Provider and pass the store */}
       <div className="App">
         <Nav />
         <Routes>
